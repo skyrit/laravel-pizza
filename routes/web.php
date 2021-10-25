1b73a1c8a504ae046dafa765f2b4a32e37da6f86
Skyrit <skyprowei728@gmail.com>
2021-10-25 17:44:23 +0800

    validation added

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/pizza', [PizzaController::class, 'index'])->name('pizza.index');
+Route::get('/pizza/create', [PizzaController::class, 'create'])->name('pizza.create');
+Route::post('/pizza/store', [PizzaController::class, 'store'])->name('pizza.store');
